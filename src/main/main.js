88c5fb85b89f86e154693ff78163bf2c80128455
natefrank48 <78034830+natefrank48@users.noreply.github.com>
2021-03-02 14:43:05 -0600

    quiz format

--- src/main/main.js
+++ src/main/main.js
@@ -1,8 +1,18 @@
-let button = 0;
+let question;
+let answer1;
+let answer2;
+let answer3;
+let answer4;
 
-function increase(){
-  button++;
-}
+question = "How would you print \"\Hello World\"?";
+
+setInterval(function update(){
+  document.getElementById('question').innerHTML = question;
+  document.getElementById('ans1').innerHTML = answer1;
+  document.getElementById('ans2').innerHTML = answer2;
+  document.getElementById('ans3').innerHTML = answer3;
+  document.getElementById('ans4').innerHTML = answer4;
+});
 
 setInterval(function update(){
   document.getElementById('button').innerHTML = button;
