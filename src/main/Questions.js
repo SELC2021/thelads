f7b4eaa45073346051e8bc4e893d0a4da2faf373
Jackson Joblinske <jjoblinske@gmail.com>
2021-03-16 14:55:37 -0500

    Added question bank function

---
+++ src/main/Questions.js
@@ -0,0 +1,5 @@
+let questionBank = ["test", "question 2"];
+let correctAns = ["correc ans","second correct answer"];
+let answersOne = ["ans1", "answer1 "];
+let answersTwo = ["ans2", "answer2"];
+let answersThree = ["ans3", "answer 3"];
--- src/resources/Questions.js
+++
@@ -1,5 +0,0 @@
-let questionBank = ["test"];
-let correctAns = ["correc ans"];
-let answersOne = ["ans1"];
-let answersTwo = ["ans2"];
-let answerThree = ["ans3"];
