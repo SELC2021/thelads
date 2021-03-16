1c1aba24e39ce0d264f70ba8ec3860c5d88974f6
natefrank48 <78034830+natefrank48@users.noreply.github.com>
2021-03-16 14:57:38 -0500

    Questions and answers

--- src/resources/Questions.js
+++ src/resources/Questions.js
@@ -1,5 +1,6 @@
-let questionBank = ["test"];
-let correctAns = ["correc ans"];
-let answersOne = ["ans1"];
-let answersTwo = ["ans2"];
-let answerThree = ["ans3"];
+let questionBank = ["How would you print \"Hello World\" to the screen?", "What would this code print?\nprint (\"I like pizza!\")", "What would this code print?\n\na=5\nb=9\nc=b-a\nprint(\"c\")", 
+"Which of the following would print \"I am 5 years old\" if userAge = 5?", "What would this code print?\n\nname = Mark\nprint(\"His name is \" + name)", "What would this code print?\n\nd=7\ne=2\nd = d * e\nprint(\"d\")"];
+let correctAns = ["print (\"Hello World\")", "I like pizza!", "4", "print (\"I am \" + userAge + \" years old\")", "His name is Mark", "14"];
+let answersOne = ["print \"Hello World\"", "\"I like pizza!\"", "c", "print (\"I am (userAge) years old\")", "His name is + name", "49"];
+let answersTwo = ["(\"Hello World\")", "\"Ilikepizza!\"", "14", "print (\"I am\" + userAge + \"years old\")", "His name is \"Mark\"", "7"];
+let answerThree = ["print (Hello World)", "Ilikepizza!", "-4", "print (\"I am \" userAge \" years old\")", "His name isMark", "9"];
