598b69f914346e88cd860aa2de5a038a3671f504
natefrank48 <78034830+natefrank48@users.noreply.github.com>
2021-03-30 15:47:28 -0500

    Even even more questions

--- src/resources/Questions.js
+++ src/resources/Questions.js
@@ -1,8 +1,9 @@
 let questionBank = ["How would you print \"Hello world\"?", "What would this code print?\nprint (\"I like pizza!\")", 
-"What would this code print?\n\na=5\nb=5\nc=b-a\nprint(c)", "Which of the following would print \"I am 5 years old\" given that userAge = 5?", 
+"What would this code print?\n\na=5\nb=9\nc=b-a\nprint(c)", "Which of the following would print \"I am 5 years old\" given that userAge = 5?", 
 "What would this code print?\n\nname = \"Mark\"\nprint (\"His name is\", name)", "What would this code print?\n\nd=7\ne=2\nd=e*d\nprint(d)", 
-"What values can a boolean variable have?", "What variable type holds only whole numbers?", "Which of the following is NOT a variable type?"];
-let correctAns = ["print (\"Hello world\")","I like pizza!", "4", "print (\"I am\", userAge, \"years old\")", "His name is Mark", "14", "true/false", "integer", "number"];
-let answersOne = ["print (Hello world)", "\"I like pizza!\"", "c", "print (\"I am \", userAge, \"years old\")", "His name isMark", "7", "numbers 1-10", "double", "char"];
-let answersTwo = ["print \"Hello world\"", "Ilikepizza!", "14", "print (\"I am \" (userAge) \" years old\")", "His name is, name", "d", "decimals between 0 and 1", "single", "boolean"];
-let answersThree = ["(\"Hello wolrd\")", "\"Ilikepizza!\"", "-4", "print (\"I am \", print(userAge), \" years old\")", "\"His name is \"Mark", "49", "a word", "string", "integer"];
+"What values can a boolean variable have?", "What variable type holds only whole numbers?", "Which of the following is NOT a variable type?", 
+"What would this code print?\n\nf=2\ng=11\nh=g+f\nprint(\"h\")"];
+let correctAns = ["print (\"Hello world\")","I like pizza!", "4", "print (\"I am\", userAge, \"years old\")", "His name is Mark", "14", "true/false", "integer", "number", "h"];
+let answersOne = ["print (Hello world)", "\"I like pizza!\"", "c", "print (\"I am \", userAge, \"years old\")", "His name isMark", "7", "numbers 1-10", "double", "char", "13"];
+let answersTwo = ["print \"Hello world\"", "Ilikepizza!", "14", "print (\"I am \" (userAge) \" years old\")", "His name is, name", "d", "decimals between 0 and 1", "single", "boolean", "22"];
+let answersThree = ["(\"Hello wolrd\")", "\"Ilikepizza!\"", "-4", "print (\"I am \", print(userAge), \" years old\")", "\"His name is \"Mark", "49", "a word", "string", "integer", "it is an error"];
