85c1bafdefe240459a95cdbd599c79964b5e2307
natefrank48 <78034830+natefrank48@users.noreply.github.com>
2021-04-20 15:02:48 -0500

    fixed

--- src/resources/Questions.js
+++ src/resources/Questions.js
@@ -5,6 +5,6 @@ let questionBank = ["How would you print \"Hello world\"?", "What would this cod
 "What would this code print?"];
 let codeBank = ["", "print (\"I like pizza!\")", "a=5<br>b=9<br>c=b-a<br>print(c)", "", "name = \"Mark\"<br>print (\"His name is\", name)", "d=7<br>e=2<br>d=e*d<br>print(d)", "", "", "", "f=2<br>g=11<br>h=g+f<br>print(\"h\")"];
 let correctAns = ["print (\"Hello world\")","I like pizza!", "4", "print (\"I am\", userAge, \"years old\")", "His name is Mark", "14", "true/false", "integer", "number", "h"];
-let answersOne = ["print (Hello world)", "\"I like pizza!\"", "c", "print (\"I am \", userAge, \" years old\")", "His name isMark", "7", "numbers 1-10", "double", "character", "13"];
-let answersTwo = ["print \"Hello world\"", "Ilikepizza!", "14", "print (\"I am \" (userAge) \" years old\")", "His name is, name", "d", "decimals between 0 and 1", "single", "boolean", "22"];
-let answersThree = ["(\"Hello wolrd\")", "\"Ilikepizza!\"", "-4", "print (\"I am \", print(userAge), \" years old\")", "\"His name is \"Mark", "49", "a word", "string", "integer", "it is an error"];
+let answersOne = ["print (Hello world)", "\"I like pizza!\"", "c", "print (\"I am \", userAge, \" years old\")", "His name isMark", "7", "numbers 1-10", "float", "String", "13"];
+let answersTwo = ["print \"Hello world\"", "Ilikepizza!", "14", "print (\"I am \" (userAge) \" years old\")", "His name is, name", "d", "decimals between 0 and 1", "boolean", "boolean", "22"];
+let answersThree = ["(\"Hello wolrd\")", "\"Ilikepizza!\"", "-4", "print (\"I am \", print(userAge), \" years old\")", "\"His name is \"Mark", "49", "a word", "String", "integer", "it is an error"];
